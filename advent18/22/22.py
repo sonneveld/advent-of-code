df89d51369134b479b3dd514522e8199a26d33a9
Nick Sonneveld <nick@sonneveld.name>
2018-12-22 20:49:02 +1100

    2018 Day 22

---
+++ advent18/22/22.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+
+import re
+import collections
+import sys
+import os
+from queue import PriorityQueue, Empty
+from functools import lru_cache
+
+DEBUG = "DEBUG" in os.environ
+
+try:
+    input_filename = sys.argv[1]
+except IndexError:
+    input_filename = "input.txt"
+
+with open(input_filename) as f:
+    data = f.read()
+
+LRU_CACHE_SIZE = 131072
+SEARCH_PAST_OFFSET = 100
+
+depth, target_x, target_y = [int(x) for x in re.findall(r'[\-\+]?[\d]+', data)]
+target = (target_x, target_y)
+mouth = (0,0)
+
+@lru_cache(maxsize=LRU_CACHE_SIZE)
+def calc_erosion_level(x, y):
+    return (calc_geological_index(x,y) + depth) % 20183
+
+@lru_cache(maxsize=LRU_CACHE_SIZE)
+def calc_geological_index(x, y):
+    pos = (x,y)
+    if pos == mouth:
+        return 0
+    if pos == target:
+        return 0
+    if y == 0:
+        return 16807*x
+    if x == 0:
+        return 48271 * y
+
+    return calc_erosion_level(x-1, y) * calc_erosion_level(x, y-1)
+
+@lru_cache(maxsize=LRU_CACHE_SIZE)
+def calc_type(x,y):
+    return calc_erosion_level(x,y) % 3
+
+# part 1
+
+risk_level = {}
+risk_level[ (0,0) ] = calc_type(0,0)
+
+for y in range(0, target[1]+1):
+    for x in range(0, target[0]+1):
+        if (x,y) == (0,0):
+            continue
+        risk_level[(x,y)] = calc_type(x,y) + risk_level.get( (x,y-1), 0) + risk_level.get( (x-1, y), 0) - risk_level.get( (x-1, y-1), 0)
+
+print(risk_level[target])
+
+
+
+# part 2
+
+type_name_lookup = ('rocky', 'wet', 'narrow')
+type_display_lookup = '.=|'
+
+tool_lookup = ( ('climbing', 'torch'), ('climbing', 'neither'), ('torch', 'neither'), )
+
+@lru_cache(maxsize=LRU_CACHE_SIZE)
+def calc_adjacent(x,y):
+    result = []
+    if x > 0:
+        result.append( (x-1, y) )
+    if x < target[0]+SEARCH_PAST_OFFSET:
+        result.append( (x+1, y) )
+    if y > 0:
+        result.append( (x, y-1) )
+    if y < target[1]+SEARCH_PAST_OFFSET:
+        result.append( (x, y+1) )
+    return result
+
+
+def dump():
+    print('''
+. rocky  - (c)limbing gear or (t)orch
+= wet    - (c)limbing gear or (n)either
+| narrow - (t)orch or (n)either''')
+
+    display_lookup = {}
+    for (k,cost) in sorted(cost_table.items()):
+        x,y,tool = k
+        at_type = type_display_lookup[calc_type(x,y)]
+        display_lookup[(x,y)] = display_lookup.get( (x,y), [at_type[:1]]) + [f'{tool[0]}{cost:04}']
+
+    for y in range(0, target[1]+1):
+        for x in range(0, target[0]+1):
+            v = ' '.join(display_lookup.get((x,y), []))
+            print(f'{v:13} | ', end = '')
+        print()
+
+
+cost_table = {}
+
+queue = PriorityQueue()
+queue.put((0, 0, 0, 'torch'))
+queue.put((7, 0, 0, 'climbing'))
+
+while True:
+
+    try:
+        cost, x, y, tool = queue.get_nowait()
+    except Empty:
+        break
+
+    cur_cost = cost_table.get( (x,y, tool) , None)
+    if cur_cost is not None and cur_cost <= cost:
+        continue
+
+    if DEBUG:
+        print(cost, x, y, tool)
+        if (x,y) == target:
+            print ("POTENTIAL ANSWER:", cost, x, y, tool)
+            dump()
+
+    cost_table[ (x,y, tool)] = cost
+
+    available_tools = tool_lookup[calc_type(x, y)]
+
+    assert tool in available_tools
+
+    adj = calc_adjacent(x,y)
+
+    for next_x, next_y in adj:
+        next_type = calc_type(next_x, next_y)
+        next_tools = tool_lookup[next_type]
+        for next_tool in next_tools:
+            if next_tool == tool:
+                queue.put(  (cost+1, next_x, next_y, next_tool) )
+            else:
+                if next_tool in available_tools:
+                    queue.put(  (cost+1+7, next_x, next_y, next_tool) )
+
+if DEBUG:
+    dump()
+
+print (cost_table[ (target[0], target[1], 'torch')])
